93a76a4baab6be4fe3eeb6fcd73e7697fea3e75f
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-04-08 19:08:00 +0200

    [add] struttura footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,14 +13,57 @@ export default {
 
 <template>
 
-  <div>
-    F
-  </div>
+  <footer>
+    <div class="row row-cols-4 ">
+      <div class="col">
+        <img class="logo-foot" src="/src/assets/img/author-logo-round-small.png" alt="">
+      </div>
+      <div class="col">
+        <h3>sdgsdfgdfgdf</h3>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quidem, id sapiente laudantium soluta consequatur repudiandae iste illo perferendis quam sunt tempore placeat? Natus iusto consequatur rem! Porro eligendi culpa placeat.</p>
+      </div>
+      <div class="col">
+        <h3>gfjfgj</h3>
+        <ul>
+          <li><a href="">fgd</a></li>
+          <li><a href="">dfg</a></li>
+          <li><a href="">fdg</a></li>
+          <li><a href="">fdsg</a></li>
+        </ul>
+      </div>
+      <div class="col">
+        <h3>gfjfgj</h3>
+        <ul>
+          <li><a href="">fgd</a></li>
+          <li><a href="">dfg</a></li>
+          <li><a href="">fdg</a></li>
+          <li><a href="">fontaweson</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
   
 </template>
 
 
 
 <style lang="scss" scoped>
+@import '../assets/scss/partials/_variables';
+footer{
+  padding: 50px;
+  font-size: 0.8rem;
+  color: $light-clr;
+  background-image: url(/src/assets/img/footer-3.jpg);
+  background-size: cover;
+  .logo-foot{
+    width: 45%;
+  }
+  li::marker {
+    font-size: 0.5rem;
+  font-family: "Font Awesome 5 Free";
+  font-weight: 800;
+  content: "\f054";
+}
+}
 
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -15,7 +15,7 @@ export default {
 <template>
 
   <div>
-   m
+   Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, esse! Dolorem, consequuntur ex autem quisquam earum perferendis aspernatur optio magnam, omnis odit nisi possimus. Eaque aut cum debitis ducimus necessitatibus.
   </div>
     
 </template>
