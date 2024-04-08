557225c0fc616db42901328729924ff7aba3ef10
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-04-08 13:02:08 +0200

    [add] bozza header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -13,15 +13,65 @@ export default {
 </script>
 
 <template>
+  <header>
 
-  <div>
-    h
-  </div>
+    <div class="container-lg ">
+      <div class="row row-cols-2 ">
+        <div class="col">
+          <img src="/src/assets/img/author-logo-round-200x205.png" alt="">
+        </div>
+        <div class="col">
+          <nav class="navbar navbar-expand">
+            <div class="container-fluid">
+              <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                  <li class="nav-item">
+                    <a class="nav-link text-white active" aria-current="page" href="#">Home</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-white " href="#">Features</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-white " href="#">Pricing</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-white " href="#">Pricing</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-white " href="#">Pricing</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-white " href="#">Pricing</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-white " href="#">Pricing</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-white " href="#">fontaweson</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </header>
   
 </template>
 
 
 
 <style lang="scss" scoped>
+header{
+  background-image: url(/src/assets/img/footer-3.jpg);
+  background-size: cover;
+  .container-lg{
+    height: 500px;
+    .collapse{
+      flex-direction: row-reverse;
+    }
+  }
+}
 
 </style>
